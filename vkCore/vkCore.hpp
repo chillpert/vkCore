5490a278503c3c4b166c56023f99fd97a746fc84
chillpert <dev@christianhilpert.com>
2020-12-28 12:34:12 +0100

    added assert macro and fixed compile errors

--- vkCore/vkCore.hpp
+++ vkCore/vkCore.hpp
@@ -1,22 +1,24 @@
+#include <iostream>
 #include <vulkan/vulkan.hpp>
 
-#define VK_ASSERT( statement, message )  if ( statement )\
-                                         {\
-                                           std::cerr << "vkCore: " << message << std::endl:\
-                                         }
+#define VK_ASSERT( statement, message )              \
+  if ( statement )                                   \
+  {                                                  \
+    std::cerr << "vkCore: " << message << std::endl; \
+  }
 
 namespace vkCore
 {
   inline vk::Device device = nullptr;
 
   /// Creates a fence with a unique handle.
-  inline auto initFenceUnique( vk::FenceCreateFlags flags = vk::FenceCreateFlagBits::eSignaled ) -> vk::UniqueFence;
+  inline auto initFenceUnique( vk::FenceCreateFlags flags = vk::FenceCreateFlagBits::eSignaled ) -> vk::UniqueFence
   {
     vk::FenceCreateInfo createInfo( flags );
 
-    UniqueFence fence = device.createFenceUnique( createInfo );
+    vk::UniqueFence fence = device.createFenceUnique( createInfo );
     VK_ASSERT( fence, "Failed to create fence." );
 
-    return fence;
+    return std::move( fence );
   }
-}
+} // namespace vkCore
